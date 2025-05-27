5d34a4a5393ea6df7bb3630f9b15690842412221
Inchara G Hegde <139611369+inchara23@users.noreply.github.com>
2025-05-27 23:27:45 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
-public class helloworld{
-  public static void main(String[] args){
-    system.out.println("Hello from Jenkins");
+public class helloworld {
+  public static void main(String[] args) {
+    System.out.println("Hello from Jenkins");
   }
 }
+
